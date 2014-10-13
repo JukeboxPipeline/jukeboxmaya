f6ee4a4348331405e8772dae1648f1b81b9c1e68
David.Zuber <david.zuber@gmx.de>
2014-10-13 18:31:03 +0200

    better documentation for the testrunner script

--- testrunner.py
+++ testrunner.py
@@ -2,7 +2,8 @@
 """This script runs all tests for jukebox-maya with the maya python interpreter
 
 Because using an external interpreter causes all kind of problems, this script
-runs the tests with mayapy.exe
+runs the tests with mayapy.
+This script will execute pytest with the given commandline arguments.
 """
 import argparse
 import os
@@ -21,12 +22,19 @@ def setup_argparse():
     """
     parser = argparse.ArgumentParser(
         description="Run tests with the maya python intepreter.\
-        py.test is used for running the tests.")
+        py.test is used for running the tests. All options will be\
+        propagated to py.test.")
     return parser
 
 
 def setup_environment():
     """Set up neccessary environment variables
+
+    This appends all path of sys.path to the python path
+    so mayapy will find all installed modules.
+    We have to make sure, that we use maya libs instead of
+    libs of the virtual env. So we insert all the libs for mayapy
+    first.
 
     :returns: None
     :rtype: None
@@ -52,9 +60,10 @@ def execute_mayapy(args):
     mayapy = osinter.get_maya_python()
     allargs = [mayapy]
     allargs.extend(args)
-    print "Executing mayapy with: %s" % allargs
+    print "Executing mayapy. This might take a while. Arguments used: %s" % allargs
     mayapyprocess = subprocess.Popen(allargs, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     (out, err) = mayapyprocess.communicate()
+    mayapyprocess.wait()  # make sure returncode is set!
     print "Process mayapy finished!"
     return mayapyprocess.returncode, out
 
@@ -74,6 +83,9 @@ def main(argv=sys.argv[1:]):
     args.extend(options)
     setup_environment()
     rc, output = execute_mayapy(args)
+    # print one line at a time, so it is less unlikely to run
+    # out of space. if the line is really long, this will fail
+    # with an IOError.
     for l in output.split("\n"):
         print l
     sys.exit(rc)
